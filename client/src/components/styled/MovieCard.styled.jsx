bb4d61451af5581ae97be573caa7932b53ed51c4
laksh <lakshyam38@gmail.com>
2022-03-26 20:19:43 +0530

    styles for the movie cards

---
+++ client/src/components/styled/MovieCard.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  height: 200px;
+  width: 65%;
+  background: gray;
+  padding: 20px;
+  margin-top: 15px;
+  display: flex;
+
+  img {
+    height: 200px;
+  }
+`;
