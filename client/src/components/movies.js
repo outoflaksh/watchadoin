a76c084f8af8c30abc965202563145b3eaf223eb
laksh <lakshyam38@gmail.com>
2022-03-26 20:21:24 +0530

    sample movie data

---
+++ client/src/components/movies.js
@@ -0,0 +1,44 @@
+const movieList = [
+  {
+    title: "Blade Runner",
+    rating: "8.1",
+    genre: "Action, Drama, Sci-Fi",
+    plot: "A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
+    cast: "Harrison Ford, Rutger Hauer, Sean Young",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    director: "Ridley Scott",
+  },
+  {
+    title: "Drive",
+    rating: "7.8",
+    genre: "Action, Drama",
+    plot: "An action drama about a mysterious Hollywood stuntman and mechanic who moonlights as a getaway driver, and finds himself in trouble when he helps out his neighbor.",
+    cast: "Ryan Gosling, Carey Mulligan, Bryan Cranston",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BZjY5ZjQyMjMtMmEwOC00Nzc2LTllYTItMmU2MzJjNTg1NjY0XkEyXkFqcGdeQXVyNjQ1MTMzMDQ@._V1_SX300.jpg",
+    director: "Nicolas Winding Refn",
+  },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    rating: "8.3",
+    genre: "Drama, Romance, Sci-Fi",
+    plot: "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.",
+    cast: "Jim Carrey, Kate Winslet, Tom Wilkinson",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTY4NzcwODg3Nl5BMl5BanBnXkFtZTcwNTEwOTMyMw@@._V1_SX300.jpg",
+    director: "Michel Gondry",
+  },
+  {
+    title: "Annihilation",
+    rating: "6.8",
+    genre: "Adventure, Drama, Horror",
+    plot: "A biologist signs up for a dangerous, secret expedition into a mysterious zone where the laws of nature don't apply.",
+    cast: "Natalie Portman, Jennifer Jason Leigh, Tessa Thompson",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTk2Mjc2NzYxNl5BMl5BanBnXkFtZTgwMTA2OTA1NDM@._V1_SX300.jpg",
+    director: "Alex Garland",
+  },
+];
+
+export default movieList;
