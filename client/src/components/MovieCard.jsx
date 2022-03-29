d56119bb067c1245dbfdba14c0e1f8346b998353
laksh <lakshyam38@gmail.com>
2022-03-29 12:34:31 +0530

    movie cards completed

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,4 +1,6 @@
-import { CardContainer } from "./styled/MovieCard.styled";
+import styled from "styled-components";
+
+import { CardContainer, Info } from "./styled/MovieCard.styled";
 import { Flex } from "./styled/Common.styled";
 
 const MovieCard = ({ movie }) => {
@@ -7,10 +9,30 @@ const MovieCard = ({ movie }) => {
     <CardContainer>
       <Flex align="center">
         <img src={movie.poster} alt="" />
-        <h1>{movie.title}</h1>
+        <Info>
+          <Flex direction="column" align="flex-start">
+            <h1>
+              {movie.title}
+              <span>IMDb {movie.rating}</span>
+            </h1>
+            <p style={genreStyles}>{movie.genre}</p>
+            <p>{movie.plot}</p>
+            <p style={castStyles}>Cast: {movie.cast}</p>
+          </Flex>
+        </Info>
       </Flex>
     </CardContainer>
   );
+};
+
+const genreStyles = {
+  fontStyle: "italic",
+  fontWeight: 300,
+};
+
+const castStyles = {
+  fontWeight: 500,
+  color: "#9AAADE",
 };
 
 export default MovieCard;
--- client/src/components/styled/MovieCard.styled.jsx
+++ client/src/components/styled/MovieCard.styled.jsx
@@ -3,11 +3,38 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   height: 240px;
   width: 100%;
-  background: gray;
-  margin-top: 15px;
+  background: #4f6190;
+  border-radius: 25px;
+  margin-top: 25px;
   padding: 20px;
 
   img {
     height: 200px;
+  }
+`;
+
+export const Info = styled.div`
+  padding: 10px 20px;
+  letter-spacing: -0.01em;
+
+  h1,
+  p {
+    color: #e6efff;
+  }
+
+  h1 {
+    font-weight: 900;
+    font-size: 2.8rem;
+
+    span {
+      color: #ffeeca;
+      font-weight: 600;
+      font-size: 1.2rem;
+      margin-left: 10px;
+    }
+  }
+
+  p {
+    margin-top: 5px;
   }
 `;
