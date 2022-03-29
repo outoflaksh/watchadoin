c6eded2ac8b4d84a9cb16f1bc8c4438eed30db81
laksh <lakshyam38@gmail.com>
2022-03-29 12:33:48 +0530

    global styles component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from "./components/styled/Global";
 
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <div className="App">
         <Pages />
       </div>
