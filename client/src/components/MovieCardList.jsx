c836e0ea04c14bb5cee060c017d75ff6dd647f08
laksh <lakshyam38@gmail.com>
2022-04-22 17:22:20 +0530

    movie array replaced by context

--- client/src/components/MovieCardList.jsx
+++ client/src/components/MovieCardList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import movieList from "./movies.js";
 import MovieCard from "./MovieCard.jsx";
+import { useContext, useEffect } from "react";
+import { MovieContext } from "../contexts/MovieContext.jsx";
 
 const MovieCardList = () => {
+  const [movies, setMovies] = useContext(MovieContext);
+
   return (
     <div style={{ width: "65%" }}>
-      {movieList.map((movie, idx) => (
+      {movies.map((movie, idx) => (
         <MovieCard movie={movie} key={idx} />
       ))}
     </div>
--- client/src/pages/Watchlist.jsx
+++ client/src/pages/Watchlist.jsx
@@ -1,11 +1,16 @@
 import MovieCardList from "../components/MovieCardList";
+import AddMovie from "../components/AddMovie";
+import { MovieProvider } from "../contexts/MovieContext";
 import { Page, Flex } from "../components/styled/Common.styled";
 
 const Watchlist = () => {
   return (
     <Page>
-      <Flex align="center" justify="center">
-        <MovieCardList />
+      <Flex align="center" justify="center" direction="column">
+        <MovieProvider>
+          <AddMovie />
+          <MovieCardList />
+        </MovieProvider>
       </Flex>
     </Page>
   );
