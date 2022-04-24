f01a0f227834e943f8a0d1d2c3ab38477974e585
laksh <lakshyam38@gmail.com>
2022-04-24 13:11:48 +0530

    removed global.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import GlobalStyles from "./components/styled/Global";
 
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyles />
       <div className="App">
         <Navbar />
         <Pages />
